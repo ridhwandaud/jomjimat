be70ab299f095f6db781102fea0e21652b99db78
ridhwan <muhammadridhwandaud@gmail.com>
2017-11-15 17:44:16 +0800

    add own module

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,9 +4,15 @@ import { Container, Content } from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import TransactionsList from './scene/TransactionsList';
 import Budget from './scene/Budget';
+import Boilerplate from 'react-native-android-toast'
 
 
 class Dashboard extends Component{
+
+	componentWillMount(){
+		Boilerplate.show('Boilerplate runs fine', Boilerplate.LONG);
+	}
+
   render(){
     return(
       <View style={{flex: 1}}>
